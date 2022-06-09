ff194a154a5cdf1614d580d79a67acf227ff6935
MunirNuristani <nuristani.munir@gmail.com>
2022-06-09 15:58:38 -0700

    firebase fixes

--- utils/firebase-config.js
+++ utils/firebase-config.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIV_FB_APIKEY,
+  apiKey: process.env.NEXT_PUBLIC_FB_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_FB_AUTHDOMAIN,
   projectId: process.env.NEXT_PUBLIC_FB_PROJECTID,
   storageBucket: process.env.NEXT_PUBLIC_FB_STORAGEBUCKET,
