c273b3e000efd3161fd06ce2bffb58cf2f255ad6
MunirNuristani <nuristani.munir@gmail.com>
2022-08-29 22:23:12 -0700

    new updates

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import {ReducerWrapper} from '../context/AppContext'
 
-
 function MyApp({ Component, pageProps }) {
   return(
     <ReducerWrapper >
+      <meta
+        name='viewport'
+        content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+      />
+      <meta name="apple-mobile-web-app-capable" content="yes"></meta>
       <Layout >
         <Component {...pageProps} />
       </Layout>
--- pages/_document.js
+++ pages/_document.js
@@ -4,52 +4,63 @@ export default function MyDocument() {
   return (
     <Html lang="fa">
       <Head>
-        <meta name="description" content="Nuristani.info" />
+        <meta name="description" content="Nuristani.info, nuristan, nuristani, afghanistan, نورستان افغانستان نورستانی زبان نورستانی کلشه الا" />
         <meta name="keywords" content="نورستانی, کلشه الا, کلښه الا, افغانستان, نورستان, nuristan, nuristani, dictionary, translation, translate" />
         <link rel="icon" href="/logo_favicon.png" />
         <link rel="apple-touch-icon" href="/logo-favicon.png"></link>
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <meta name='application-name' content='PWA App' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <meta name='apple-mobile-web-app-title' content='PWA App' />
-        <meta name='description' content='PWA application' />
-        <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='msapplication-config' content='/icons/browserconfig.xml' />
-        <meta name='msapplication-TileColor' content='#2B5797' />
-        <meta name='msapplication-tap-highlight' content='no' />
-        <meta name='theme-color' content='#000000' />
-        <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-        <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
-        <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
-        <link rel='shortcut icon' href='/favicon.ico' />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:url' content='https://nuristani.info' />
-        <meta name='twitter:title' content='PWA App' />
-        <meta name='twitter:description' content='Best PWA App in the world' />
-        <meta name='twitter:image' content='https://nuristani.info/icons/android-chrome-192x192.png' />
-        <meta name='twitter:creator' content='@DavidWShadow' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='PWA App' />
-        <meta property='og:description' content='Best PWA App in the world' />
-        <meta property='og:site_name' content='PWA App' />
-        <meta property='og:url' content='https://nuristani.info' />
-        <meta property='og:image' content='https://nuristani.info/icons/apple-touch-icon.png' />
-        {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />  */}
-        <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-        <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-        <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-        <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-        <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-        <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-        <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
+        <meta name="application-name" content="PWA App" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+<meta name="apple-mobile-web-app-title" content="PWA App" />
+<meta name="description" content="Best PWA App in the world" />
+<meta name="format-detection" content="telephone=no" />
+<meta name="mobile-web-app-capable" content="yes" />
+<meta name="msapplication-config" content="/icons/browserconfig.xml" />
+<meta name="msapplication-TileColor" content="#2B5797" />
+<meta name="msapplication-tap-highlight" content="no" />
+<meta name="theme-color" content="#000000" />
+
+<link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+<link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+
+<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+<link rel="manifest" href="/manifest.json" />
+<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:url" content="https://nuristani.info" />
+<meta name="twitter:title" content="PWA App" />
+<meta name="twitter:description" content="Best PWA App in the world" />
+<meta name="twitter:image" content="https://nuristani.info/icons/android-chrome-192x192.png" />
+<meta name="twitter:creator" content="@DavidWShadow" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="PWA App" />
+<meta property="og:description" content="Nuristani.info" />
+<meta property="og:site_name" content="Nuristani" />
+<meta property='og:type' content='website' />
+<meta property='og:title' content='PWA App' />
+<meta property='og:description' content='Best PWA App in the world' />
+<meta property='og:site_name' content='PWA App' />
+<meta property='og:url' content='https://nuristani.info' />
+<meta property='og:image' content='https://nuristani.info/icons/apple-touch-icon.png' />
+{/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />  */}
+<link href="splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+<link href="splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+<link href="splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="splashscreens/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+<link href="splashscreens/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="splashscreens/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
       </Head>
       <body>
         <title> نهاد فرهنگی میرزا تازه گل خان </title>
