dc7b05bced3acab4f0a78fddafd0fbc852f15be6
MunirNuristani <nuristani.munir@gmail.com>
2023-01-03 08:28:57 -0800

    recent updates

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -15,14 +15,14 @@ function AricleDetail({ listOfArticles, error }) {
   const [sortedImages, setSortedImages] = useState([])
   useEffect(() => {
     dispatch({ type: "LOADINGPAGE", payload: false })
-    setSortedImages(articleToDisplay[0]?.fields.Pictures?.sort((a,b)=> a.filename>b.filename?1:(a.filename<b.filename?-1:0)))
+    setSortedImages(articleToDisplay[0]?.fields.Pictures?.sort((a, b) => a.filename > b.filename ? 1 : (a.filename < b.filename ? -1 : 0)))
   }, [listOfArticles])
   console.log(sortedImages)
   return (
     <>
       {error ? <SomethingWentWrong /> :
         loadingPage ? <LoadingPage /> : (
-          <div dir='rtl' className="container mt-10 md:mt-[120px] flex flex-col justify-center mx-auto backdrop-blur-sm bg-white/90 drop-shadow-xl p-12 rounded-xl max-w-[900px] md:max-w-[700px] sm:max-w-[360px] text-xl relative">
+          <div dir='rtl' className="container my-10 md:mt-[120px] flex flex-col justify-center mx-auto backdrop-blur-sm bg-white/90 drop-shadow-xl p-12 rounded-xl max-w-[900px] md:max-w-[700px] sm:max-w-[360px] text-xl relative">
             <button className="absolute top-5 left-5" onClick={() => {
               router.back()
               dispatch({ type: "LOADINGPAGE", payload: true })
@@ -31,17 +31,34 @@ function AricleDetail({ listOfArticles, error }) {
             <h2 className='text-2xl text-gray-500'>نویسنده: {articleToDisplay[0]?.fields.Author_Name}</h2>
 
             {articleToDisplay[0]?.fields.Pictures && (
-              <div className="flex flex-row wrap relative justify-center m-2" style={{ width: '100%', height: '50vh', position: 'relative' }}>
-                <Image src={articleToDisplay[0]?.fields.Pictures[0].url} alt='article-images' width={100} height={100} layout="fill" objectFit="contain" />
+              <div 
+                className="flex flex-row wrap relative justify-center m-2" 
+                style={{ width: '100%', height: '50vh', position: 'relative' }}>
+                <Image 
+                src={articleToDisplay[0]?.fields.Pictures[0].url} 
+                alt='article-images' 
+                width={100} 
+                height={100} 
+                layout="fill" 
+                objectFit="contain" />
               </div>)}
             <div className='text-justify my-10 text-2xl'
               dangerouslySetInnerHTML={{ __html: articleToDisplay[0]?.fields.Article_body }}
             />
             <>
               {articleToDisplay[0]?.fields.Pictures?.slice(1, articleToDisplay[0]?.fields.Pictures.length).map((img, idx) => (
-                
-                <div key={idx} className="flex flex-column justify-center m-2" style={{ width: "100%", height: '50vh', position: 'relative' }}>
-                  <Image src={img.url} l alt='article-image' width={100} height={100} layout="fill" objectFit="contain" />
+                <div
+                  key={idx}
+                  className="flex flex-column justify-center m-2"
+                  style={{ width: "100%", height: '50vh', position: 'relative' }}
+                >
+                  <Image
+                    src={img.url}
+                    alt='article-image'
+                    width={100}
+                    height={100}
+                    layout="fill"
+                    objectFit="contain" />
                 </div>))}
             </>
           </div>)}
